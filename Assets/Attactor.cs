787a2b433b5fa01d49d81e16a70bdc6831602776
FolkAkaranan <poh.yossawin@gmail.com>
2024-03-11 16:28:09 +0700

    wtf

--- Assets/Attactor.cs
+++ Assets/Attactor.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Attactor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody rb;
+    private const float G = 6.674f;
+
+    private int x = 0;
+    public static List<Attactor> Attactors;
+
+    private void FixedUpdate()
     {
-        
+        foreach (var attactor in Attactors)
+        {
+            if (true != this)
+            {
+                Attract(attactor);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
+        if (Attactors == null)
+        {
+            Attactors = new List<Attactor>();
+        }
+        Attactors.Add(this);
+    }
+
+    void Attract(Attactor other)
+    {
+        Rigidbody rb2 = other.rb;
         
+        //F G(m1*m2)/r"2
+        
+        //find dictance between 2 object (get both magnitude and direction)
+        Vector3 direction = rb.position - rb2.position;
+
+        float distance = direction.magnitude;
+        
+        //find force between onjects
+        float forceManitude = G * (rb.mass * rb2.mass) / Math.Pow(distance,2);
+        Vector3 finalforce = direction.normalized forceManitude;
+        
+        rb2.AddForce(finalforce);
+
     }
 }
